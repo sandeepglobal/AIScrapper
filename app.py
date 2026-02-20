a2ddd2eebba191de8d03a54823911b95f39eb5b4
sandeepglobal <sandeep@giinfotech.ae>
2026-02-20 13:00:38 +0400

    Update app.py

--- app.py
+++ app.py
@@ -34,19 +34,11 @@ def scrape_booking(destination, checkin, checkout, adults=2, pages=1):
     service = Service("/usr/bin/chromedriver") 
     #service = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
     options.add_argument("--start-maximized")
     options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--ignore-certificate-errors")
-    #options.add_argument("--disable-gpu") # Extra stability for cloud
+
     driver = webdriver.Chrome(service=service, options=options)
     wait = WebDriverWait(driver, 15)
-    wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
-    print(driver.page_source)
 
     driver.get(url)
     time.sleep(5)
@@ -280,6 +272,7 @@ if run_button:
     #     mime="text/csv"
     #  )
     
+
 
 
 
